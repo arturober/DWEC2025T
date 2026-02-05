f95527f0b09635891bbfa7f7db654323c173d2b7
Arturo Bernal <arturober@gmail.com>
2026-02-05 18:05:06 +0100

    ssr

--- U2 - Angular/Parte 6/angular-products-ssr/src/app/app.config.ts
+++ U2 - Angular/Parte 6/angular-products-ssr/src/app/app.config.ts
@@ -2,20 +2,21 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { baseUrlInterceptor } from './shared/interceptors/base-url-interceptor';
 import { provideSignalFormsConfig } from '@angular/forms/signals';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)),
-    provideHttpClient(withInterceptors([baseUrlInterceptor])),
+    provideHttpClient(withInterceptors([baseUrlInterceptor]), withFetch()),
     provideSignalFormsConfig({
       classes: {
         'is-valid': ({state}) => state().touched() && state().valid(),
         'is-invalid': ({state}) => state().touched() && state().invalid()
       }
-    }),
+    }), provideClientHydration(withEventReplay()),
   ]
 };
