3d322ed13837b04459870aad4b98ffc40227b63c
Arturo Bernal <arturober@gmail.com>
2026-02-13 14:23:52 +0100

    mapas

--- U2 - Angular/Parte 6/angular-maps/src/app/app.ts
+++ U2 - Angular/Parte 6/angular-maps/src/app/app.ts
@@ -2,13 +2,20 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OlMap } from './ol-maps/ol-map';
 import { OlMarker } from './ol-maps/ol-marker';
+import { GaAutocomplete } from './ol-maps/ga-autocomplete';
+import { SearchResult } from './ol-maps/search-result';
 
 @Component({
   selector: 'app-root',
-  imports: [OlMap, OlMarker],
+  imports: [OlMap, OlMarker, GaAutocomplete],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   coordinates = signal<[number, number]>([-0.5, 38.5]);
+
+  changePlace(result: SearchResult) {
+    this.coordinates.set(result.coordinates);
+    console.log(result.address); // Habría que guardarlo
+  }
 }
---
+++ U2 - Angular/Parte 6/angular-maps/src/app/ol-maps/ga-autocomplete.spec.ts
@@ -0,0 +1,8 @@
+import { GaAutocomplete } from './ga-autocomplete';
+
+describe('GaAutocomplete', () => {
+  it('should create an instance', () => {
+    const directive = new GaAutocomplete();
+    expect(directive).toBeTruthy();
+  });
+});
